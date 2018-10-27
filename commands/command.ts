a0324de71e25b57c8f59938a42d91b552abd00f6
Bemoty <signup.bemoty@outlook.com>
2018-10-27 21:56:55 +0200

    + invalid args

--- commands/command.ts
+++ commands/command.ts
@@ -1,5 +1,11 @@
 import {Message} from "discord.js";
+import ContentBot, {log4js} from "../index";
+import {Logger} from "log4js";
 
 export default interface DiscordCommand {
     onCommand(msg: Message, args: string[]);
+}
+
+export function sendInvalidArgsMessage(msg: Message, syntax: string, logger: Logger) {
+    msg.channel.send(':x: Ungültige Argumente! Syntax: ' + ContentBot.delimiter + syntax).catch(logger.error);
 }
